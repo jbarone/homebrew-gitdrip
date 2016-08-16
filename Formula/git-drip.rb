4bbbceefdedcd9db66db6232d8ba26e89b93186e
Joshua Barone <joshua.barone@gmail.com>
2016-08-16 11:57:38 -0500

    bump version

--- Formula/git-drip.rb
+++ Formula/git-drip.rb
@@ -2,7 +2,7 @@ class GitDrip < Formula
   desc "A collection of Git extensions to provide high-level repository operations for the git-drip workflow"
   homepage "https://github.com/jbarone/gitdrip"
   url "https://github.com/jbarone/gitdrip/archive/v0.2.0.tar.gz"
-  version "0.1.0"
+  version "0.2.0"
   sha256 "b63c3d3e008387363cbd2a89976ce5731712beefcc9c424864ab9b896510d818"
 
   resource "completion" do
